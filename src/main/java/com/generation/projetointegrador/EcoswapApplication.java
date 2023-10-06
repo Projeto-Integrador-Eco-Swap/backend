6584398f028d16964c08751ddab4da9df0d7d6ee
unknown <beatrizcaos@hotmail.com>
2023-10-06 13:24:52 -0300

    Project Remake - até 14md

---
+++ src/main/java/com/generation/projetointegrador/EcoswapApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projetointegrador/EcoswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
